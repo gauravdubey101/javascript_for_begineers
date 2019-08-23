0d42435c4f5913eaedade5bcd46610a87c95c1d5
gaurav Dubey <gauravd638@gmail.com>
2019-08-23 23:12:41 +0530

    inital commits

---
+++ js/index.js
@@ -0,0 +1,21 @@
+console.log('javalearning');
+//variables in js.
+//var ,let,const.
+ var name ='gaurav';
+ var channel;
+ var marks = 34;
+ console.log(name,channel,marks);
+
+ //rules for createing javasricpt
+ //can't start with number
+ //can start with letter ,number,_ and &
+ //are case sensitive
+
+ var $city ='delhi';
+ console.log($city);
+
+ const myname = 'gaurav dev';
+ //const variblename; cant do this 
+ 
+ console.log(myname);
+
