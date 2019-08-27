3c831ece6941e7e0016056a84bb55d77acd1723b
gaurav Dubey <gauravd638@gmail.com>
2019-08-27 14:46:26 +0530

    new js

--- js/index.js
+++ js/index.js
@@ -18,4 +18,23 @@ console.log('javalearning');
  //const variblename; cant do this 
  
  console.log(myname);
+ //what is lit ?
+ //basically it work inside block level(can form local variable)
+{
+    let city = 'rampur';
+    console.log(city);
+}
+//array
+
+const arr1 =[1,2,3,4,5];
+arr1.push(45);
+console.log(arr1);
+
+/* common test case using in javascript
+1. camleCase
+2. kebab-case
+3. snake_case
+4. PascalCase
+
+*/
 
---
+++ js/tut2.js
@@ -0,0 +1,6 @@
+//primitive data type
+
+//string
+var name = "gaurav";
+console.log("my string is " +name);
+console.log("type of data type" + (typeof name))
